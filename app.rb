af90d96e7e05c1ea5b1dcd0f2ffbe4ee875db924
Y0rTz <skylarking420@gmail.com>
2017-06-18 15:10:23 +0200

    Adds #GET #POST endpoints.

--- app.rb
+++ app.rb
@@ -1,18 +1,20 @@
 require 'bundler'
 require 'roda'
+
 module Blockchain
   class App < Roda
     plugin :json, classes: [Array, Hash]
     route do |r|
-      r.get do
-        r.on "hello" do
-          {hello: :world}
-        end
-      end
-
-      r.post do
-        r.on "create" do
-          {params: r.params}
+      r.on "api" do
+        r.on "v1" do
+          r.is "accounts" do
+            r.get do
+              {accounts: :get_accounts}
+            end
+            r.post do
+              {accounts: r.params["address"]}
+            end
+          end
         end
       end
     end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
-
 describe Blockchain::App, roda: :app do
-  describe '/hello' do
-    before { get('/hello') }
+  describe '#GET /accounts' do
+    before { get('/api/v1/accounts') }
 
     it { expect(last_response).to be_successful }
-    it { expect(JSON.parse(last_response.body)).to eq ({ "hello" => "world" })}
+    it { expect(JSON.parse(last_response.body)).to eq ({ "accounts" => "get_accounts" })}
   end
 
-  describe '/create' do
-    before { post('/create', {something: "hello world"}) }
+  describe '#POST /accounts' do
+    before { post('/api/v1/accounts', { address: '123456' }) }
 
     it { expect(last_response).to be_successful }
-    it { expect(JSON.parse(last_response.body)).to eq ({"params"=>{"something"=>"hello world"}})}
+    it { expect(JSON.parse(last_response.body)).to eq ({ "accounts" => "123456" })}
   end
 end
 
